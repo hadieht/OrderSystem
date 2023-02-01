09e4a25028db91a686dc2315628c9ce4d7b6fc7c
Hadi Ehterami <hadi.ehterami@gmail.com>
2023-02-01 19:04:46 +0100

    add cancel validator

---
+++ src/Application/Order/Commands/CancelOrder/CancelOrderCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace Application.Order.Commands.CancelOrder;
+
+public class CancelOrderCommandValidator : AbstractValidator<CancelOrderCommand>
+{
+    public CancelOrderCommandValidator()
+    {
+        RuleFor(a => a.OrderNumber).NotNull().NotEmpty();
+    }
+}

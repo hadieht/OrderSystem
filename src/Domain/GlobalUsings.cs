c5ebb58f9688bfe1702ea27c61c194d00c082ac4
Hadi Ehterami <hadi.ehterami@gmail.com>
2023-02-01 18:55:16 +0100

    fix

--- src/Domain/GlobalUsings.cs
+++ src/Domain/GlobalUsings.cs
@@ -1,6 +1,2 @@
-﻿//global using Project2.Domain.Common;
-//global using Project2.Domain.Entities;
-global using Domain.Enums;
-//global using Project2.Domain.Events;
-//global using Project2.Domain.Exceptions;
+﻿global using Domain.Enums;
 global using Domain.ValueObjects;
---
+++ tests/Domain.UnitTests/Entities/OrderTests.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entities;
+using Domain.ValueObjects;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderSystem.Domain.UnitTests.Entities
+{
+    public class OrderTests
+    { 
+
+        [Test]
+        [TestCase("orderNumber")]
+        [TestCase("orderDate")]
+        [TestCase("customerName")]
+        [TestCase("customerEmail")]
+        [TestCase("address")]
+        public void Order_ThrowsException(string parameterName)
+        {
+            var datetime = DateTime.Now;
+            var exception = Assert.Throws<NotSupportedException>(() =>
+                            new Order(parameterName == "orderNumber" ? null : "number",
+                                               datetime,
+                                               parameterName == "customerName" ? null : "name",
+                                               parameterName == "customerEmail" ? null : new Mock<Email>().Object,
+                                               parameterName == "address" ? null : new Mock<Address>().Object));
+
+            Assert.AreEqual(parameterName, exception?.Message);
+        }
+    }
+}
